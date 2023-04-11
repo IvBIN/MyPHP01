6df4816f1381dda2a18a37a5c63a474e442cfe21
User <bin444@mail.ru>
2023-04-11 20:37:20 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+//$text = 'Пивет мир.';
+//
+//echo $text;
+//echo 'Это код на PHP';
+//?>
+<!---->
+<!--<div>-->
+<!--    <b>HTML продолжает</b> свою работу-->
+<!--</div>-->
+<!---->
+<?php
+//$text = 'мир!';
+//
+//echo '<strong>Привет</strong>';
+//?>
+<!---->
+<!--<i>-->
+<!--    --><?php //echo $text; ?>
+<!--</i>-->
+<!---->
+<?php
+//$text = 'мир!';
+//
+//echo '<strong>Привет</strong>' ;
+//?>
+<!---->
+<!--<i>-->
+<!--    --><?php //= $text ?>
+<!--</i>-->
+
+<?php
+//echo "Привет мир!";
+//$one = "Первая строчка";
+//$two = "Вторая строчка";
+//$three = "Третья строчка";
+//?>
+<!---->
+<!--<ul>-->
+<!--    <li>--><?php //echo $one; ?><!--</li>-->
+<!--    <li>--><?php //= $two; ?><!--</li>-->
+<!--    <li>--><?php //echo $three; ?><!--</li>-->
+<!--</ul>-->
+
+<?php
+//$str_1 = "Привет";
+//$str_2 = "этот";
+//$str_3 = "мир!";
+//
+//echo "<ul>
+//    <li>$str_1</li>
+//    <li>$str_2</li>
+//    <li>$str_3</li>
+//</ul>"
+//?>
+<!---->
+<?php
+////$num_1 =6;
+////$num_2 =2;
+////$num_3 =7;
+////$num_4 =15;
+////$num_5 =-5;
+////echo $num_1 + $num_2 + $num_3 + $num_4 + $num_5;
+////echo  1012 * 0.3;
+//echo 2 ** 15;
+//?>
+
+<?php
+//$a = 1;
+//$b = 2;
+//echo ($a<=1 and $b>=3) ? $a + $b : $a - $b;
+//?><!-- //#5-->
+
+<?php
+//$a = 10;
+//$b = 7;
+//echo (($a>2 and $a<11) and ($b>=6 and $b<14)) ? "Верно" : "Неверно";
+//?><!-- //#6-->
+
+<?php
+$a = $_POST['a'];
+$b = $_POST['b'];
+$operation = "divide";
+$isAvailableCalculation = true;
+$result = $a / $b;
+
+?>
+<form action="index.php" method="post">
+    <?php if($isAvailableCalculation == false){
+        echo "Вычисления не доступны";
+    }
+    else{
+        echo "Вычисления доступны";
+    }?>
+    <p><input type="text" name="a" placeholder="Введите первое число"></p>
+    <p><input type="text" name="b" placeholder="Введите второе число"></p>
+    <p><select>
+        <option>+</option>
+        <option>-</option>
+        <option selected value="$operation">/</option>
+        <option>*</option>
+    </select></p>
+    <p><input type="submit" value="Выполнить"></p>
+</form>
+<h1><?= $result; ?></h1>
